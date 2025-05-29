602cdf3fd873cccf45dc5cd8a9c1d5a8376b436b
HotdogEnthusiast <cichaj@protonmail.com>
2025-05-29 11:04:10 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os, discord
+from discord.ext import commands
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
