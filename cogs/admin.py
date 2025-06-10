f0987b20c1be2c4c49b9ce39ea58d794a560765c
Cichaj <em.rvso@gmail.com>
2025-06-10 17:42:23 +0200

    Removed admin cog, as there's no use for it at the moment.

--- cogs/admin.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-
-class admin(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-async def setup(bot):
-    await bot.add_cog(admin(bot))
-    print("Admin Cog loaded successfully.")
